ab2f50f5395d76b8aeecc412e2d880cf25794f3a
TashmiOshera <osheratashmi@gmail.com>
2026-03-20 23:25:45 +0530

    index.js change

--- OrderManagementService/index.js
+++ OrderManagementService/index.js
@@ -13,7 +13,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3000', // frontend URL
   'http://127.0.0.1:3000', 
-  'http://localhost:5173',// another dev URL
+  'http://localhost:5173',
   // add other allowed origins here
 ];
 
