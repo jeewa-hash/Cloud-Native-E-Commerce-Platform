b54e3c95f5c17cae9f4f02b60492fb178bec1b8b
jeewa-hash <jkumarasekara@gmail.com>
2026-03-23 13:13:04 +0530

    Add API Gateway config and update service URLs

---
+++ Frontend/src/config.js
@@ -0,0 +1,6 @@
+const API_GATEWAY = "https://0eqk5p25gi.execute-api.us-east-1.amazonaws.com";
+
+export const AUTH_SERVICE_URL = `${API_GATEWAY}/auth`;
+export const SHOP_SERVICE_URL = `${API_GATEWAY}/shop`;
+export const ORDER_SERVICE_URL = `${API_GATEWAY}/order`;
+export const NOTIFICATION_SERVICE_URL = `${API_GATEWAY}/notification`;
