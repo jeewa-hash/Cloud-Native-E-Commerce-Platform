981e0dc24be2ff313dbd8287e68e31f2fe33279b
jeewa-hash <jkumarasekara@gmail.com>
2026-03-23 16:28:52 +0530

    update API config with correct service URLs and fix import paths

--- Frontend/src/apiConfig.js
+++ Frontend/src/apiConfig.js
@@ -2,5 +2,6 @@ const API_GATEWAY = "https://0eqk5p25gi.execute-api.us-east-1.amazonaws.com";
 
 export const AUTH_API = `${API_GATEWAY}/auth`;
 export const SHOP_SERVICE_URL = `${API_GATEWAY}/shop`;
-export const ORDER_SERVICE_URL = `${API_GATEWAY}/order`;
+export const ORDER_API = `${API_GATEWAY}/order`;
 export const NOTIFICATION_SERVICE_URL = `${API_GATEWAY}/notification`;
+export const PRODUCTS_API = `${API_GATEWAY}/order`;
