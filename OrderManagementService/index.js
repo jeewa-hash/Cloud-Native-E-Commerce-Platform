b0e65095693fcb2ec6837e1e80654e23f7082912
TashmiOshera <osheratashmi@gmail.com>
2026-03-23 16:57:46 +0530

    update CORS to allow Swagger UI on ALB

--- OrderManagementService/index.js
+++ OrderManagementService/index.js
@@ -22,7 +22,9 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://127.0.0.1:3000',
   'http://localhost:5173',
-  'http://order-frontend-bucket-123.s3-website.eu-north-1.amazonaws.com'
+  'http://localhost:4000',
+  'http://order-frontend-bucket-123.s3-website.eu-north-1.amazonaws.com',
+  'http://orderservice-alb-1335748857.eu-north-1.elb.amazonaws.com'
 ];
 
 app.use(
