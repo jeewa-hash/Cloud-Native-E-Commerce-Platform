f6a3db6ed811afeb65c16fd8f663b55d36d1dcfc
Chaveen Sewni <shewnichaveen@gmail.com>
2026-04-26 02:27:30 +0530

    deliveryProdIntegration

--- Frontend/src/config.jsx
+++ Frontend/src/config.jsx
@@ -1,9 +1,9 @@
 export default {
-    DELIVERY_API: "http://localhost:5003",
+    DELIVERY_API: "http://delivery-service-alb-1864288318.eu-north-1.elb.amazonaws.com/delivery",
     ORDER_API: "http://orderservice-alb-1335748857.eu-north-1.elb.amazonaws.com/api/order",
     USER_API: "http://auth-alb-1878950287.us-east-1.elb.amazonaws.com/api/users",
-    PRODUCTS_API: "http://localhost:4040/api/products",
-    SHOP_API: "http://localhost:4040/api/shops",
+    PRODUCTS_API: "http://shop-alb-883151970.eu-north-1.elb.amazonaws.com/api/products",
+    SHOP_API: "http://shop-alb-883151970.eu-north-1.elb.amazonaws.com/api/shops",
    
     DELIVERY_PORT: '5003',
     ORDER_PORT: '4000',
